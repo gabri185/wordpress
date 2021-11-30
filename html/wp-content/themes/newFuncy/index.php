fb6f21071e617b7af60350d59f475a09c4f47af7
gabri185 <gdominguezreyes@danielcastelao.org>
2021-11-30 10:56:19 +0100

    thumbnails

--- html/wp-content/themes/newFuncy/index.php
+++ html/wp-content/themes/newFuncy/index.php
@@ -1,21 +1,43 @@
 <!doctype html>
 <html>
 <head>
+
     <meta charset="utf-8">
     <title>NEW FUNCY</title>
-    <link rel="stylesheet" href="<?php get_stylesheet_uri();?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
 </head>
 <body>
 
+<?php define( 'WP_USE_THEMES', false )?>
 <?php get_header("header")?>
 
+
+
+
 <div id="cuerpo">
+
     <?php
+        echo get_stylesheet_uri();
 
         echo "<h1 class='morado'>HOLA FUNCY</h1>";
-        echo get_stylesheet_uri();
-    ?>
+
+        global $post;
+        if (have_posts()):
+            while (have_posts()):
+                the_post();
+                the_title();
+                the_content();
+                the_post_thumbnail();
+
+                echo"<br>";
+            endwhile;
+        endif;
+        ?>
+
 </div>
-<?php get_footer()?>
+
+
 </body>
 </html>
+
+<?php get_footer()?>
